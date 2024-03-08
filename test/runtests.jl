e0323105f9410724ae7580692b27f05f7012702b
odow <o.dowson@gmail.com>
2024-03-08 17:11:13 +1300

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,16 +11,30 @@ import Gurobi_jll
     @test Gurobi_jll.is_available()
 end
 
-@testset "check_path_is_string" begin
+@testset "libgurobi" begin
     @test Gurobi_jll.libgurobi_path isa String
-    @test Gurobi_jll.gurobi_cl_path isa String
-    @test Gurobi_jll.grbgetkey_path isa String
+    majorP, minorP, technicalP = Ref{Cint}(), Ref{Cint}(), Ref{Cint}()
+    @ccall Gurobi_jll.libgurobi.GRBversion(
+        majorP::Ptr{Cint},
+        minorP::Ptr{Cint},
+        technicalP::Ptr{Cint},
+    )::Cvoid
+    # Update these values when you update Artifacts.toml
+    @test majorP[] == 11
+    @test minorP[] == 0
+    @test technicalP[] == 0
 end
 
 @testset "gurobi_cl" begin
+    @test Gurobi_jll.gurobi_cl_path isa String
     contents = sprint() do io
         return run(pipeline(`$(Gurobi_jll.gurobi_cl()) -v`, stdout = io))
     end
-    @show contents
-    @test occursin("Gurobi Optimizer", contents)
+    if !Sys.iswindows()
+        @test occursin("Gurobi Optimizer", contents)
+    end
+end
+
+@testset "grbgetkey" begin
+    @test Gurobi_jll.grbgetkey_path isa String
 end
