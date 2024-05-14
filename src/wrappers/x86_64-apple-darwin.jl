c6a8c5cf7b7e6c26ac87b257a10a26464134d2c2
odow <o.dowson@gmail.com>
2024-05-15 09:43:01 +1200

    Fix codesigning issue on x86_64-apple-darwin

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -14,6 +14,11 @@ JLLWrappers.@declare_executable_product(gurobi_cl)
 JLLWrappers.@declare_executable_product(grbgetkey)
 
 function __init__()
+    # This is needed to work-around a permission issue on some intel macs.
+    # See Gurobi.jl#545 for details.
+    libgurobi_path = joinpath(artifact_dir, "lib", "libgurobi110.dylib")
+    run(`codesign --remove-signature $libgurobi_path`)
+    # Back to the standard header
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libgurobi,
@@ -22,6 +27,7 @@ function __init__()
     )
     JLLWrappers.@init_executable_product(gurobi_cl, "bin/gurobi_cl")
     JLLWrappers.@init_executable_product(grbgetkey, "bin/grbgetkey")
+    # The standard license supports only Python installs. Remove it.
     gurobi_lic = joinpath(artifact_dir, "lib", "gurobi.lic")
     if isfile(gurobi_lic)
         rm(gurobi_lic; force = true)
