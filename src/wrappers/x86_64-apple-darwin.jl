ba1c22de006ec65d80f5c7bb83293e76e048cdec
torressa <23246013+torressa@users.noreply.github.com>
2024-10-02 18:01:01 +0100

    Update with v12.0.0 libraries

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -7,7 +7,7 @@ export gurobi_cl, grbgetkey, libgurobi
 
 JLLWrappers.@generate_wrapper_header("Gurobi")
 
-JLLWrappers.@declare_library_product(libgurobi, "@rpath/libgurobi110.dylib")
+JLLWrappers.@declare_library_product(libgurobi, "@rpath/libgurobi120.dylib")
 
 JLLWrappers.@declare_executable_product(gurobi_cl)
 
@@ -17,12 +17,12 @@ function __init__()
     JLLWrappers.@generate_init_header()
     # This is needed to work-around a permission issue on some intel macs.
     # See Gurobi.jl#545 for details.
-    libgurobi_path = joinpath(artifact_dir, "lib", "libgurobi110.dylib")
+    libgurobi_path = joinpath(artifact_dir, "lib", "libgurobi120.dylib")
     run(`codesign --remove-signature $libgurobi_path`)
     # Back to the standard header
     JLLWrappers.@init_library_product(
         libgurobi,
-        "lib/libgurobi110.dylib",
+        "lib/libgurobi120.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
     JLLWrappers.@init_executable_product(gurobi_cl, "bin/gurobi_cl")
