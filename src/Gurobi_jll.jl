381ba0a9e6bc63458733c7338af098310fb15fdd
Oscar Dowson <odow@users.noreply.github.com>
2026-05-06 20:26:57 +1200

    Add support for v13.0.2 (#40)

--- src/Gurobi_jll.jl
+++ src/Gurobi_jll.jl
@@ -10,6 +10,8 @@ using Base
 using Base: UUID
 import JLLWrappers
 
+const GUROBI_DIR = "gurobi1302"
+
 JLLWrappers.@generate_main_file_header("Gurobi")
 
 JLLWrappers.@generate_main_file("Gurobi", UUID("c018c7e6-a5b0-4aea-8f80-9c1ef9991411"))
